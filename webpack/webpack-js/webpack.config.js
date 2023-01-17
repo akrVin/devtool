3f9c2813d1bafcea3080004a65f9482b69ffcc6c
akrVin <akrvin@yandex.ru>
2023-01-17 11:54:38 +0500

    installed html webpack plugin

--- webpack/webpack-js/webpack.config.js
+++ webpack/webpack-js/webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
-
 const devMode = mode === 'development';
-
 const target = devMode ? 'web' : '.browserslist';
 const devtool = devMode ? 'source-map' : undefined;
 
@@ -16,5 +15,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: 'index.[contenthash].js',
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
 }

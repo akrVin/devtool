11345f38b9e2a03ff6159c01ea6d3058d66847d4
akrVin <akrvin@yandex.ru>
2023-01-24 13:42:22 +0500

    обработка less и scss с ошибкой

--- gulp/gulp-js/gulp/config/path.js
+++ gulp/gulp-js/gulp/config/path.js
@@ -7,12 +7,20 @@ const srcFolder = './src';
 
 export const path = {
   build: {
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
     files: `${buildFolder}/files/`
   },
   src: {
+    less: `${srcFolder}/less/style.less`,
+    //scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
   },
   watch: {
+    less:`${srcFolder}/less/**/*.less`,
+    //scss:`${srcFolder}/scss/**/*.scss`,
+    html:`${srcFolder}/**/*.html`,
     files: `${srcFolder}/files/**/*.*`
   },
   clean: buildFolder,
--- gulp/gulp-js/gulpfile.js
+++ gulp/gulp-js/gulpfile.js
@@ -2,23 +2,37 @@
 import gulp from "gulp";
 // импорт путей
 import { path } from "./gulp/config/path.js";
+// импорт общих плагинов
+import { plugins } from "./gulp/config/plugins.js";
 
 // передаем значение в глобальную переменную
 global.app = {
   path: path,
-  gulp: gulp
+  gulp: gulp,
+  plugins: plugins,
 }
 
 // импорт задач
 import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
+// import { scss } from "./gulp/tasks/scss.js";
+import { less } from "./gulp/tasks/less.js";
 
 // наблюдатель и изменнение файлов
 function watcher() {
-  gulp.watch(path.watch.files, copy)
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  // gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.less, less);
 }
 
+const mainTasks = gulp.parallel(copy, html, less);
+
+
 // построение сценариев построения задачи
-const dev = gulp.series(copy, watcher);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 // выполнение задачи по умолчанию
-gulp.task('default', copy);
+gulp.task('default', dev);
